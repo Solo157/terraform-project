3f9a90a5a13167c726724355878f59c2bb1c05c4
Васюков Кирилл Леонидович <kirill.vasyukov@eltex.loc>
2026-09-19 11:44:50 +0700

    env

---
+++ env/staging/main.tf
@@ -0,0 +1,5 @@
+module "infrastructure" {
+  source = "../../modules/infrastructure"
+
+  vm_name = var.vm_name
+}
---
+++ env/staging/variables.tf
@@ -0,0 +1,9 @@
+variable "folder_id" {
+  description = "Yandex Cloud folder ID"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the VM"
+  type        = string
+}
